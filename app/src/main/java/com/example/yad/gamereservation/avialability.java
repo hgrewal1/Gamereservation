3d71d290060e617409ebf01af563fbf4dfbb9dcd
hgrewal1 <harvinder.grewal@hotmail.com>
2017-06-18 00:41:12 -0400

    spinner adapter

--- app/src/main/java/com/example/yad/gamereservation/avialability.java
+++ app/src/main/java/com/example/yad/gamereservation/avialability.java
@@ -1,19 +1,47 @@
 package com.example.yad.gamereservation;
 
 import android.accounts.Account;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class avialability extends AppCompatActivity {
 
+    private String TAG = search.class.getSimpleName();
+    ArrayAdapter<String> adapter;
+    private ProgressDialog pDialog;
+    ArrayList<String> arylist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +52,10 @@ public class avialability extends AppCompatActivity {
 
         actionBar.setTitle("Availability");
         setupNavigationView();
+
+        arylist = new ArrayList<>();
+
+        new MyTask().execute();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -83,4 +115,135 @@ public class avialability extends AppCompatActivity {
                     }
                 });
     }
-}
+
+    private class MyTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(avialability.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            URL url = null;
+
+            Intent intent=getIntent();
+            String r=intent.getStringExtra("name");
+
+            try {
+                url = new URL("http://192.168.1.8:8080/gameservervation/cegepgim/gamereservation/viewallgamestation");
+
+                HttpURLConnection client = null;
+                client = (HttpURLConnection) url.openConnection();
+                client.setRequestMethod("GET");
+                int responseCode = client.getResponseCode();
+                System.out.println("\n Sending 'GET' request to url:" + url);
+                System.out.println("\n Response code:" + responseCode);
+                InputStreamReader myInput = new InputStreamReader(client.getInputStream());
+                BufferedReader in = new BufferedReader(myInput);
+                String inputline;
+                StringBuffer response = new StringBuffer();
+
+
+                while ((inputline = in.readLine()) != null) {
+                    response.append(inputline);
+                }
+                in.close();
+
+
+
+                JSONObject obj = new JSONObject(response.toString());
+                String status=obj.getString("Status");
+                if (status.equals("ok")) {
+                    JSONArray ary = new JSONArray();
+                    ary = obj.getJSONArray("GameStations");
+                    adapter = new ArrayAdapter<String>(avialability.this, android.R.layout.simple_spinner_item);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    for (Integer i = 0; i < ary.length(); i++) {
+                        JSONObject obj1 = ary.getJSONObject(i);
+                        String o2 = obj1.getString("GameStationId");
+                        String o4 = obj1.getString("GameStation");
+                        String o5 = obj1.getString("LocationId");
+                        adapter.add(o4);
+
+                    }
+                }
+                else {
+                    Log.e(TAG, "Couldn't get json from server.");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Couldn't get json from server. Check LogCat for possible errors!",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } catch (final JSONException e) {
+                Log.e(TAG, "Json parsing error: " + e.getMessage());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Json parsing error: " + e.getMessage(),
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+
+            // Spinner adapter
+            mySpinner
+                    .setAdapter(adapter);
+
+            // Spinner on item click listener
+            mySpinner
+                    .setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                        @Override
+                        public void onItemSelected(AdapterView<?> arg0,
+                                                   View arg1, int position, long arg3) {
+                            // TODO Auto-generated method stub
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> arg0) {
+                            // TODO Auto-generated method stub
+                        }
+                    });
+        }
+        }
+
+
+    }
+
