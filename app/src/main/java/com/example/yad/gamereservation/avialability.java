c124ebfb1196e7e0184fdbfdb7e884f5fb1aa6d4
hgrewal1 <harvinder.grewal@hotmail.com>
2017-06-18 02:54:55 -0400

    spinner adapter

--- app/src/main/java/com/example/yad/gamereservation/avialability.java
+++ app/src/main/java/com/example/yad/gamereservation/avialability.java
@@ -1,6 +1,10 @@
 package com.example.yad.gamereservation;
 
 import android.accounts.Account;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -17,6 +21,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -34,14 +40,19 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 
-public class avialability extends AppCompatActivity {
+public class avialability extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private String TAG = search.class.getSimpleName();
+    private String TAG = avialability.class.getSimpleName();
     ArrayAdapter<String> adapter;
     private ProgressDialog pDialog;
     ArrayList<String> arylist;
+    String a,b,r,url;
+    EditText editText;
+    Spinner spinner2,spinner3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,14 +60,69 @@ public class avialability extends AppCompatActivity {
         setupNavigationView();
         ActionBar actionBar=getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(false);
-
-        actionBar.setTitle("Availability");
-        setupNavigationView();
-
+       spinner2=(Spinner) findViewById(R.id.spinner2);
+        spinner3=(Spinner) findViewById(R.id.spinner3);
+        editText=(EditText) findViewById(R.id.date);
         arylist = new ArrayList<>();
 
         new MyTask().execute();
+
+        actionBar.setTitle("Availability");
+        setupNavigationView();
+        List<String> items= new ArrayList<String>();
+        items.add("9:00 am");
+        items.add("10:00 am");
+        items.add("11:00 am");
+        items.add("12:00 pm");
+        items.add("1:00 pm");
+        items.add("2:00 pm");
+        items.add("3:00 pm");
+        items.add("4:00 pm");
+        items.add("5:00 pm");
+        items.add("6:00 pm");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(dataAdapter);
+        ArrayAdapter<String> dataAdapter2= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(dataAdapter);
+        editText.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new avialability.DatePickerFragment();
+                newFragment.show(getFragmentManager(), "datePicker");
+            }
+
+        });
+
     }
+public  void available(View view){
+    url="http://192.168.1.8:8080/gameservervation/cegepgim/gamereservation/viewavailability"+r+"&"+a+"&"+b;
+    new MyTask2().execute();
+}
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        spinner2=(Spinner)parent;
+        spinner3=(Spinner)parent;
+        if(spinner2.getId()==R.id.spinner2){
+
+        a=parent.getSelectedItem().toString();}
+if (spinner3.getId()==R.id.spinner3){
+
+    b=parent.getSelectedItem().toString();
+}
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -222,17 +288,15 @@ public class avialability extends AppCompatActivity {
             Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
 
             // Spinner adapter
-            mySpinner
-                    .setAdapter(adapter);
+            mySpinner.setAdapter(adapter);
 
             // Spinner on item click listener
-            mySpinner
-                    .setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
                         @Override
-                        public void onItemSelected(AdapterView<?> arg0,
+                        public void onItemSelected(AdapterView<?> parent,
                                                    View arg1, int position, long arg3) {
-                            // TODO Auto-generated method stub
+                            r=parent.getItemAtPosition(position).toString();
 
                         }
 
@@ -243,7 +307,126 @@ public class avialability extends AppCompatActivity {
                     });
         }
         }
+    private class MyTask2 extends AsyncTask<Void, Void, Void> {
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(avialability.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+
+
+
+            try {
+                URL url1 = new URL(url);
+
+                HttpURLConnection client = null;
+                client = (HttpURLConnection) url1.openConnection();
+                client.setRequestMethod("GET");
+                int responseCode = client.getResponseCode();
+                System.out.println("\n Sending 'GET' request to url:" + url);
+                System.out.println("\n Response code:" + responseCode);
+                InputStreamReader myInput = new InputStreamReader(client.getInputStream());
+                BufferedReader in = new BufferedReader(myInput);
+                String inputline;
+                StringBuffer response = new StringBuffer();
+
+
+                while ((inputline = in.readLine()) != null) {
+                    response.append(inputline);
+                }
+                in.close();
+
+
+
+                JSONObject obj = new JSONObject(response.toString());
+                String status=obj.getString("Status");
+                if (status.equals("ok")) {
+
+                        String o2 = obj.getString("GameStationId");
+
+
+              Intent intent=new Intent(getApplicationContext(),grewal.class);
+                    startActivity(intent);
+
+                }
+                else {
+                    Log.e(TAG, "Couldn't get json from server.");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Couldn't get json from server. Check LogCat for possible errors!",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } catch (final JSONException e) {
+                Log.e(TAG, "Json parsing error: " + e.getMessage());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Json parsing error: " + e.getMessage(),
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+
+        }
+    }
+    public class DatePickerFragment  extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return  new DatePickerDialog(getActivity(),this,year,month,day);
+        }
+
+        public void onDateSet(DatePicker view, int year,
+                              int monthOfYear, int dayOfMonth) {
+            // set day of month , month and year value in the edit text
+            editText.setText(dayOfMonth + "-"
+                    + (monthOfYear + 1) + "-" + year);
+
+        }
+    }
 
     }
 
